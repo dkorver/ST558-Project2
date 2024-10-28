cdd6d73c4ee83416cecff606074f41e762283485
dkorver <danekorver@outlook.com>
2024-10-27 20:03:54 -0400

    adding to the quarto document

--- DataDig.R
+++ DataDig.R
@@ -28,7 +28,16 @@ fg_missed <- nfl_plays %>%
 
 summary(fg_missed$kick_distance)
 ggplot(data=fg_missed,aes(x=kick_distance)) +
-  geom_bar(stat="count")
+  geom_bar(stat="count") +
+  labs(title = "Histogram of Field Goals 'Made'",
+       subtitle = "for the years 2009-2018",
+       x = "Kick Distance (in yards)",
+       y = "Count") +
+  theme_minimal() +
+  theme(plot.title=element_text(hjust = 0.5),
+        plot.subtitle=element_text(hjust = 0.5),
+        axis.line = element_line(colour = "grey50"))+
+  scale_x_continuous(breaks = 0:65)
 
 fg_blocked <- nfl_plays %>% 
   filter(play_type=="field_goal" & field_goal_result=="blocked")
