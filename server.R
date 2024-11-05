e2b9584b2ef0a75078a4d051b5eb94c9f7f31059
dkorver <danekorver@outlook.com>
2024-11-05 16:41:03 -0500

    adding shiny app

---
+++ server.R
@@ -0,0 +1,75 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(tidyverse)
+library(readr)
+library(DT)
+library(rsconnect)
+nfl_plays <- readr::read_csv(file = "NFL Play by Play 2009-2018 (v5).csv",show_col_types = FALSE)
+
+# Define server logic to summarize and view selected dataset ----
+server <- function(input, output) {
+  
+  InputDataset <- reactive({
+    nfl_plays
+  })
+  
+  # Return the requested dataset ----
+  # Note that we use eventReactive() here, which depends on
+  # input$update (the action button), so that the output is only
+  # updated when the user clicks the button
+  datasetInput <- eventReactive(input$update, {
+    switch(input$dataset,
+           "nfl" = nfl_plays,
+    )
+  }, ignoreNULL = FALSE)
+  
+  # Generate a list of x-axis variables ----
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    newdata <- nfl_plays %>% select(input$xcol,input$ycol,input$byvar)
+    newdata
+  })
+  
+  #Generate graph based on user input
+  output$barPlot <- renderPlot({
+    #get data
+    nflData <- selectedData()
+    
+    ggplot(nflData, aes(x=input$xcol, y=input$ycol, color=as.factor(input$byvar))) + geom_point(shape=1)
+  })
+  
+  output$summaryList <- renderPrint({
+    datasel <- selectedData()
+    summary(datasel)
+  })
+  
+  # Generate a summary of the dataset ----
+  output$summary <- renderPrint({
+    dataset <- InputDataset()
+    summary(dataset)
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(InputDataset(), file, row.names = FALSE)
+    }
+  )
+  
+  output$ex1 <- DT::renderDataTable(
+    DT::datatable(InputDataset(), options = list(pageLength = 5))
+  )
+  
+}
+
---
+++ ui.R
@@ -0,0 +1,89 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(readr)
+library(rsconnect)
+
+nfl_plays <- readr::read_csv(file = "NFL Play by Play 2009-2018 (v5).csv",show_col_types = FALSE)
+vars <- setdiff(names(nfl_plays), "nfl")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  navbarPage(
+    "Project 2",
+    id = "main_navbar",
+    tabPanel("About",
+             br(),
+             h4("My First R Shiny App!"),
+             div(img(src = "nfl_logo.png"),style = "text-align: left;"),
+             "Image Source: nfl.com", 
+             br(),
+             br(),
+             "This R Shiny App is using the data set from the", a(href = "https://www.kaggle.com/datasets/maxhorowitz/nflplaybyplay2009to2016/data", " Detailed NFL Play-by-Play Data 2009-2018"),"from Kaggle.com.  
+  There are two different pages in this app.    
+  There is a 'Data Exploration' page where you can look at the first few observations in the dataset and summary statistics of all the variables.  
+  There is a 'Data Download' page where you can download and view a subset of the dataset.",
+             br(),
+             br(),
+    ),  
+    
+    tabPanel("Data Exploration",
+             # App title ----
+             titlePanel("NFL play-by-play Data"),
+             # Sidebar layout with input and output definitions ----
+             sidebarLayout(
+               # Sidebar panel for inputs ----
+               sidebarPanel(
+                 # Input: Select a dataset ----
+                 selectInput("dataset", "Choose a dataset:",
+                             choices = c("nfl")),
+                 br(),
+                 h4("Choose variables to create some classical stats and graph them"),
+                 selectInput('xcol', 'X Variable', vars),
+                 selectInput('ycol', 'Y Variable', vars, selected = vars[[2]]),
+                 selectInput('byvar', 'Classification Variable', vars, selected = vars[[3]]),
+                 br(),
+               ),
+               # Main panel for displaying outputs ----
+               mainPanel(
+                 h4("Graph of selected variables"),
+                 plotOutput("barPlot"),
+                 # Output: Header + summary of distribution ----
+                 h4("Summary of selected varibles"),
+                 verbatimTextOutput("summaryList"),
+               )
+               
+             )
+    ),
+    
+    tabPanel("Data Download",
+             # App title ----
+             titlePanel("Downloading Data"),
+             # Sidebar layout with input and output definitions ----
+             sidebarLayout(
+               # Sidebar panel for inputs ----
+               sidebarPanel(
+                 # Input: Choose dataset ----
+                 selectInput("dataset", "Choose a dataset:",
+                             choices = c("nfl")),
+                 # Button
+                 downloadButton("downloadData", "Download")
+               ),
+               # Main panel for displaying outputs ----
+               mainPanel(
+                 # Output: Header + table of distribution ----
+                 h4("Observations"),
+                 DT::dataTableOutput('ex1')
+               )
+             )
+    )
+    
+  )
+)
